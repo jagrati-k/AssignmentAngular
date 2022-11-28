9294f032e5afe25f06b993b40c8611562f56b7ec
jagrati_k <jagratikhatri98@gmail.com>
2022-11-28 20:18:17 +0530

    hjg

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-app.use(express.static( path.join(__dirname, 'src','my-assignment-jags-frontend') ));
+app.use(express.static( path.join(__dirname, 'angular-client' ,'src') ));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'src','my-assignment-jags-frontend','index.html'));
+res.sendFile(path.join(__dirname,'angular-client','src','index.html'));
 });
 
 // Start the app by listening on the default Heroku port
