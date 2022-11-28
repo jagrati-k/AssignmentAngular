372a2e1aa2b0837721553a382814bb712324a89c
jagrati_k <jagratikhatri98@gmail.com>
2022-11-28 20:53:40 +0530

    message

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  socket = io.listen(process.env.PORT);
   enableProdMode();
 }
 
