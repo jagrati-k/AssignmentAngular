5e6cf22b9c351ce7b6957337d250f05b16b89169
jagrati_k <jagratikhatri98@gmail.com>
2022-11-28 20:43:18 +0530

    hjg

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.static( path.join(__dirname, '/src') ));
     
 // res.sendFile(path.join(__dirname,'src','index.html'));
 // });
-
+    
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
