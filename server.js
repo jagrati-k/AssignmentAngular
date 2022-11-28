8d98ac2e060bf6fee619cb920e2f3dbb6d1ec64e
jagrati_k <jagratikhatri98@gmail.com>
2022-11-28 20:24:40 +0530

    hjg

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 
 // Serve only the static files form the dist directory
 
-app.use(express.static( path.join(__dirname, 'angular-client' ,'src') ));
+app.use(express.static( path.join(__dirname, '/src') ));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'angular-client','src','index.html'));
-});
+// res.sendFile(path.join(__dirname,'src','index.html'));
+// });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
